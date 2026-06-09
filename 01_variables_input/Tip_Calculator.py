b36ad651aed175e5c376e1996b1bfd9ea102c3f3
k-sam592k6 <k.sam592k6@gmail.com>
2026-06-09 15:34:49 +0530

    Update Tip_Calculator.py

--- 01_variables_input/Tip_Calculator.py
+++ 01_variables_input/Tip_Calculator.py
@@ -1,1 +1,14 @@
+bill = float(input("Enter bill amount: "))
+tip_ptage = float(input("Enter tip percentage: "))
+npeople = int(input("Number of people: "))
 
+tip_amount = bill * (tip_ptage / 100)
+total_bill = bill + tip_amount
+
+print("---------------------------------")
+print(f"Bill Amount      : {bill:.2f}")
+print(f"Tip%             : {tip_ptage}%")
+print(f"Tip Amount       : {tip_amount:.2f}")
+print(f"Total Bill       : {total_bill:.2f}")
+print(f"Each Person Pays : {total_bill / npeople:.2f}")
+print("---------------------------------")
