4cc29f1e0712939cc223af2c4afb8ac7ddda02e0
k-sam592k6 <k.sam592k6@gmail.com>
2026-06-11 08:26:38 +0530

    Update Sieve (prime finder).py

--- 02_Loops/Sieve (prime finder).py
+++ 02_Loops/Sieve (prime finder).py
@@ -1,1 +1,16 @@
+n = int(input("Enter a number:"))
 
+sieve = [True]*(n+1)
+sieve[0] = False
+sieve[1] = False
+
+for i in range(2,n+1):
+    if sieve[i] == True:
+        for j in range(i*i,n+1,i):
+            sieve[j] = False
+    
+primes = [for i in range(n+1) if sieve[i] == True]
+
+print("Primes upto {n}")
+print(*primes)
+print(len(primes))
