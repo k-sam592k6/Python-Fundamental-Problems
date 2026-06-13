757d2697d85c77fc5a29dcfdad26cf9b0bc4ef25
k-sam592k6 <k.sam592k6@gmail.com>
2026-06-13 16:38:19 +0530

    Update Second Largest.py

--- 03_Collections/Second Largest.py
+++ 03_Collections/Second Largest.py
@@ -1,12 +1,15 @@
-mylist =  [int(x) for x in input("Enter numbers (space separated):").split()]
+mylist = [int(x) for x in input("Enter numbers (space separated): ").split()]
 
 max1 = max2 = float('-inf')
 
 for num in mylist:
     if num > max1:
-        max1 = max2 
-        max1 = num
-    elif num >  max2 and num != max1:
+        max2 = max1    # old largest becomes second
+        max1 = num     # new largest
+    elif num > max2 and num != max1:
         max2 = num
 
-print(max2)
+if max2 == float('-inf'):
+    print("No second largest exists!")
+else:
+    print(f"Second largest: {max2}")
