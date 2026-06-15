280e1d5a33e212fadfcd79043f6e78b5274e3b2e
k-sam592k6 <k.sam592k6@gmail.com>
2026-06-15 12:47:48 +0530

    Update Binary Search Recursive.py

--- 05_Recursions/Binary Search Recursive.py
+++ 05_Recursions/Binary Search Recursive.py
@@ -1,1 +1,23 @@
+def binary_search(arr, target, left, right):
 
+    if left > right:
+        return -1
+
+    mid = (left + right) // 2
+
+    if arr[mid] == target:
+        return mid
+
+    elif target < arr[mid]:
+        return binary_search(arr, target, left, mid - 1)
+
+    else:
+        return binary_search(arr, target, mid + 1, right)
+
+
+arr = list(map(int, input("Enter sorted numbers separated by spaces: ").split()))
+target = int(input("Enter target: "))
+
+result = binary_search(arr, target, 0, len(arr) - 1)
+
+print(result)
