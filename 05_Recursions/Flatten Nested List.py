a6ecb305ef13dccfad3530c4f779fe085f0077be
k-sam592k6 <k.sam592k6@gmail.com>
2026-06-15 15:38:33 +0530

    Create Flatten Nested List.py

---
+++ 05_Recursions/Flatten Nested List.py
@@ -0,0 +1,11 @@
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result += flatten(item)
+        else:
+            result.append(item)
+    return result
+
+nested = [1, [2, [3, 4]], 5]
+print(flatten(nested))
